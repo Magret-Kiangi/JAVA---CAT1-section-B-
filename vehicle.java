12dade5b8f2fbc20a882bda6fd23f4754dfe1b14
Magret-Kiangi <kalondumagret557@gmail.com>
2026-02-20 18:50:06 +0000

    Create vehicle.java

---
+++ vehicle.java
@@ -0,0 +1,21 @@
+//Kiangi Magret Kalondu
+//CT100/G/22803/24
+
+//Class Vehicle
+class Vehicle {
+    protected String brand;
+    protected String model;
+    protected int year;
+
+    public Vehicle(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+}
